622fd16b36041c6f954aa9355abda883658346f8
ocramz <zocca.marco@gmail.com>
2020-12-09 22:29:28 +0100

    add loadAndProcess

--- src/Text/Html.hs
+++ src/Text/Html.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# language LambdaCase #-}
 {-# options_ghc -Wno-unused-imports #-}
 module Text.Html where
 
@@ -8,7 +9,9 @@ import Control.Monad (MonadPlus (..), join, void)
 import Control.Monad.State.Lazy (MonadState (..), StateT (..), evalStateT, execStateT, runStateT)
 import Data.Bifunctor (Bifunctor (..))
 import Data.Char (digitToInt, isAlpha, isDigit, isPunctuation)
-import qualified Data.Text as T (Text, pack)
+import Data.Functor.Contravariant (Contravariant(..))
+import Data.String (IsString(..))
+
 import Data.Void
 
 -- megaparsec
@@ -20,26 +23,64 @@ import Text.Megaparsec.Pos (Pos, SourcePos, initialPos, mkPos)
 
 -- bytestring
 import qualified Data.ByteString.Lazy as LBS (ByteString, readFile)
+-- containers
+import qualified Data.Map as M (Map)
 -- lens
-import Control.Lens (Prism, (^..), (...), traverseOf)
+import Control.Lens (Traversal', Prism, (^?), (^..), (...), over, (.~))
 -- profunctors
 import Data.Profunctor.Choice (Choice(..))
+-- text
+import qualified Data.Text as T (Text, pack)
+import qualified Data.Text.Lazy as TL (toStrict, fromStrict)
 
-import Text.XML (Document, Element(..), Node(..))
-import Text.Xml.Lens (AsHtmlDocument(..), _HtmlDocument, html, name)
+import Text.XML (Document(..), Element(..), Node(..), parseLBS, def, renderText)
+import Data.XML.Types (Name)
+import Text.Xml.Lens (AsHtmlDocument(..), _HtmlDocument, html, name, text, texts)
 import Text.Xml.Lens.LowLevel (_NodeContent)
 
 
 -- >>> let quasiXml = "<html><br><br></html>" :: BL.ByteString
--- >>> quasiXml ^.. html...name
+-- >>> quasiXml ^.. html ... name
+-- ["br","br"]
 
-htmly :: LBS.ByteString
-htmly = "<html><br><br></html>"
+doc :: LBS.ByteString
+doc = "<html><div>asdf</div><div>chuck</html>"
 
-baz = htmly ^.. html ... name
+baz = doc ^.. html . texts
 
 -- traverseOf _NodeContent
 --   :: Applicative f => (Text -> f Text) -> Node -> f Node
+
+
+
+-- | all NodeContent's
+nodeContents :: Applicative f => (T.Text -> f T.Text) -> Element -> f Element
+nodeContents = elemNodes . nodeContent
+
+elemNodes :: Traversal' Element Node
+elemNodes f (Element en ea ens) = Element <$> pure en <*> pure ea <*> traverse f ens
+
+nodeContent :: Traversal' Node T.Text
+nodeContent f = \case
+  NodeContent t -> NodeContent <$> f t
+  x -> pure x
+
+loadAndProcess :: FilePath -> IO T.Text
+loadAndProcess fp = do
+  bs <- LBS.readFile fp
+  case parseLBS def bs of
+    Left e -> error $ show e -- FIXME
+    Right (Document dpre el dpost) -> do
+      el' <- flip nodeContents el $ \t ->
+        case parsePattern t of
+          Right fpInner -> loadAndProcess fpInner
+          Left _ -> pure t
+      let
+        doc' = Document dpre el' dpost
+        tl = renderText def doc'
+      pure $ TL.toStrict tl
+
+
 
 parseOrPass :: Applicative f =>
                (FilePath -> f T.Text) -- load, parse, expand references and flatten
